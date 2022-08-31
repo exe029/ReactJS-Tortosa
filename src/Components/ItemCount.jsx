cfdd96fd660b6f7cd2ebf46afa85e65414aec370
exe029 <exe029@hotmail.com>
2022-08-30 23:27:30 -0300

    des4.2

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -10,24 +10,24 @@ const ItemCount = ({ onAdd, initial, stock }) => {
       };
     
       return (
-      <div className="flex flex-col justify-center items-center py-2 px-3">
+      <div className="flex flex-col justify-center items-center py-4 px-4  rounded-md shadow-md bg-gradient-to-r from-violet-300 ">
         <div className="flex flex-row justify-center items-center w-22 h-20 font-medium text-2xl  border-b-4 border-t-4 border-l-4 border-r-4 border-violet-500">
             <button
-              className="text-lg text-left border-solid border-b-2 pb-1 m-3 py-1 px-2 bg-violet-600 capitalize text-white rounded-full shadow-sm cursor-pointer hover:shadow-md hover:bg-violet-500 transition active:bg-violet-400 active:shadow-none"
+              className="disabled:opacity-40 text-lg text-left border-solid border-b-2 pb-1 m-3 py-1 px-2 bg-violet-600 capitalize text-white rounded-full shadow-sm cursor-pointer hover:shadow-md hover:bg-violet-500 transition active:bg-violet-400 active:shadow-none"
               onClick={() => addProduct(-1)}
               disabled={qty === initial}>
               -
             </button>
             <span className="text-center">{qty}</span>
             <button
-            className="text-lg text-left border-solid border-b-2 pb-1 m-3 py-1 px-2 bg-violet-600 capitalize text-white rounded-full shadow-sm cursor-pointer hover:shadow-md hover:bg-violet-500 transition active:bg-violet-400 active:shadow-none"
+            className="disabled:opacity-40 text-lg text-left border-solid border-b-2 pb-1 m-3 py-1 px-2 bg-violet-600 capitalize text-white rounded-full shadow-sm cursor-pointer hover:shadow-md hover:bg-violet-500 transition active:bg-violet-400 active:shadow-none"
             onClick={() => addProduct(+1)}
             disabled={qty === stock}>
             +
             </button>
 
           <button
-          className=" bg-violet-600 font-bold text-base mt-5 mb-5 cursor-pointer"
+          className=" bg-violet-600 font-bold text-base mt-5 mb-5 cursor-pointer disabled:opacity-40"
           onClick={() => {
           onAdd(qty);}}
           disabled={stock === 0 ? true : null}>
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ItemListContainer = ({ title }) => {
     return ( 
-        <div className="flex-col py-4 text-center bg-gradient-to-r from-violet-500">
+        <div className="flex-col py-4 text-center bg-gradient-to-b from-violet-500 shadow-md">
             <h1 className="font-bold text-xl font-serif">{title}</h1>
             <p className="text-lg">
             ¡Es hora de jugar!
