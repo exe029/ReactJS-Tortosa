a1c2513b529e3b91e61ae781f6a96c7f720c8901
exe029 <exe029@hotmail.com>
2022-08-30 22:35:51 -0300

    des4.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import NavBar from './Components/NavBar';
 import ItemListContainer from './Components/ItemListContainer';
-import ItemCount from './components/ItemCount';
+import ItemCount from './Components/ItemCount';
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
